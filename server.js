9103824f7a62e3ac65a94c137b2b63505024ab45
JessmannPengard <contacto@jessmann.com>
2023-04-19 16:31:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tienda_paypal'
+});
+connection.connect((err) => {
+    if (err) {
+        console.log("No se ha podido establecer la conexión. Error: " + err);
+        return;
+    }
+    console.log("Conexión establecida correctamente");
+});
+
+app.get("/", (req, res) => {
+    connection.query("SELECT * FROM tblproductos", (err, results, fields) => {
+        if (err) {
+            console.log(err);
+            let response = {
+                error: err
+            }
+            res.send(JSON.stringify(response));
+        }
+        res.send(JSON.stringify(results));
+    });
+});
+
+app.listen(3500, () => { console.log("Servidor escuchando en 3500") });
